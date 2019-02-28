c2731da58a7f1dea228333dbbbe1592e41cc1bdc
almirpask <almirpask@hotmail.com>
2019-02-27 23:14:27 -0300

    Initial Commit

---
+++ signos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signos
+//
+//  Created by Almir Santos on 27/02/19.
+//  Copyright © 2019 Almir Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
